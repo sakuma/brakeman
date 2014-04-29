811e4489953fddaa6179b3c4e609f28fed94ab04
Justin Collins <justin@presidentbeef.com>
2014-04-28 18:48:44 -0700

    Support before_action alias of before_filter

--- test/apps/rails4/app/controllers/users_controller.rb
+++ test/apps/rails4/app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def test_sql_sanitize
     User.where("age > #{sanitize params[:age]}")
   end
+
+  def test_before_action
+    render @page
+  end
+
+  before_action :set_page
+
+  def set_page
+    @page = params[:page]
+  end
 end
